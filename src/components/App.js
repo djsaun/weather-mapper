9c819a333d25b4523b250afd7733c2fc0dc4a749
David Saunders <david@monkee-boy.com>
2018-09-24 12:03:39 -0500

    Store weather details in state on map click

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,42 @@ class App extends Component {
         county: '',
         state: '',
         zip: '' 
+      },
+      weather: {
+        // Feels like temp
+        apparentTemp: '',
+        // Actual temp
+        temp: '',
+        // The daytime high temperature
+        tempHigh: '',
+        // The overnight low temperature
+        tempLow: '',
+        // Relative humidity
+        humidity: '',
+        // A machine-readable text summary of this data point, suitable for selecting an icon for display
+        icon: '',
+        // Probability of precipitation occurring, between 0 and 1
+        precipProbability: '',
+        // Type of precipitation occuring currently
+        precipType: '',
+        // A human-readable text summary of this data point
+        summary: '',
+        // The UNIX time of when the sun will rise during a given day
+        sunriseTime: '',
+        // The UNIX time of when the sun will set during a given day
+        sunsetTime: '',
+        // The UNIX time at which this data point begins
+        time: '',
+        // The wind gust speed in miles per hour
+        windGust: '',
+        // The wind speed in miles per hour.
+        windSpeed: '',
+        units: 'imperial'
       }
     }
 
     this.getCoordinates = this.getCoordinates.bind(this);
+    this.getWeatherFromCoordinates = this.getWeatherFromCoordinates.bind(this);
     this.getLocationFromCoordinates = this.getLocationFromCoordinates.bind(this);
   }
 
@@ -29,11 +61,13 @@ class App extends Component {
   getCoordinates(map, e) {
       this.setState({
         location: {
+          ...this.state.location,
           latitude: JSON.stringify(e.lngLat.lat),
           longitude: JSON.stringify(e.lngLat.lng)
         }
       });
 
+      this.getWeatherFromCoordinates();
       this.getLocationFromCoordinates();
   }
 
@@ -42,15 +76,43 @@ class App extends Component {
     .then(res => {
       const location = {...res.data.results[0].locations[0]};
 
-      console.log(location);
-
       this.setState({
         address: {
+          ...this.state.address,
           street: location.street,
           city: location.adminArea5,
           county: location.adminArea4,
           state: location.adminArea3,
           zip: location.postalCode
+        }
+      })
+    })
+  }
+
+  getWeatherFromCoordinates() {
+    axios.get(`https://api.darksky.net/forecast/${process.env.REACT_APP_DARKSKY_KEY}/${this.state.location.latitude},${this.state.location.longitude}`)
+    .then(res => {
+      const currentWeather = res.data;
+
+      console.log(currentWeather);
+
+      this.setState({
+        weather: {
+          ...this.state.weather,
+          apparentTemp: currentWeather.currently.apparentTemperature,
+          temp: currentWeather.currently.temperature,
+          tempHigh: currentWeather.daily.data[0].temperatureHigh,
+          tempLow: currentWeather.daily.data[0].temperatureLow,
+          humidity: currentWeather.currently.humidity,
+          icon: currentWeather.currently.icon,
+          precipProbability: currentWeather.currently.precipProbability,
+          precipType: currentWeather.currently.precipType,
+          summary: currentWeather.currently.summary,
+          sunriseTime: currentWeather.daily.data[0].sunriseTime,
+          sunsetTime: currentWeather.daily.data[0].sunsetTime,
+          time: currentWeather.currently.time,
+          windGust: currentWeather.currently.windGust,
+          windSpeed: currentWeather.currently.windSpeed,
         }
       })
     })
